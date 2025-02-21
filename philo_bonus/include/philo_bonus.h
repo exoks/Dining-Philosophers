4165c191f364e852b326892c29c2adc7b1a6f54a
Oussama Ezzaou <oezzaou@c2r1p9.1337.ma>
2025-02-21 12:09:09 +0100

    philo_bonus: philo_bonus.h has been cleaned

--- philo_bonus/include/philo_bonus.h
+++ philo_bonus/include/philo_bonus.h
@@ -9,8 +9,12 @@
 /*   Updated: 2023/05/21 17:02:13 by oezzaou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//====<[ Include guards: ]>=====================================================
 #ifndef PHILO_BONUS_H
 # define PHILO_BONUS_H
+
+//====<[ Headers: ]>============================================================
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
@@ -19,9 +23,9 @@
 # include <semaphore.h>
 # include <signal.h>
 
-//###############################
-//#            MENU             #
-//###############################
+//====<[ Macros: ]>=============================================================
+
+//====<[ Macros: menu ]>==========================
 # define PROGRAM_NAME "philo_bonus"
 # define ARG_1 "[number_of_philosophers]"
 # define ARG_2 "[time_to_die]"
@@ -37,36 +41,33 @@
 # define SUCCESS_ACTION  "On success, The simualtion starts,"
 # define SUCCESS_REASON "Until a philo die or reash max_meals."
 
-//###############################
-//#          ACTIONS            #
-//###############################
+//====<[ Macros: actions ]>=======================
 # define EAT "%llu %d is eating\n"
 # define THINK "%llu %d is thinking\n"
 # define SLEEP "%llu %d is sleeping\n"
 # define DIE "%llu %d died\n"
 # define TAKE_FORK "%llu %d has taken a fork\n"
 
-//##############################
-//#        EXIT_STATUS         #
-//##############################
+//====<[ Macros: exit status ]>===================
 # define FAILURE 1
 # define SUCCESS 0
 
-//##############################
-//#       LOGICAL VALUES       #
-//##############################
+//====<[ Macros: logical values ]>================
 # define FALSE 0
 # define TRUE 1
 # define DIED 0
-//##############################
-//#         SIMAPHORES         #
-//##############################
+
+//====<[ Macros: simaphores ]>====================
 # define SEM_PRINTER "sem_print"
 # define SEM_MEALS "sem_meals"
 # define SEM_FORKS "sem_forks"
 
+//====<[ Types: ]>==============================================================
 typedef unsigned long long int	t_ullint;
 
+//====<[ Structs: ]>============================================================
+
+//====<[ Struct: s_fork ]>========================
 typedef struct t_args
 {
 	int				philos_nbr;
@@ -76,18 +77,21 @@ typedef struct t_args
 	int				max_meals;
 }					t_args;
 
+//====<[ Struct: t_general ]>=====================
 typedef struct s_general
 {
 	sem_t			*printer;
 	sem_t			*meals;
 }					t_general;
 
+//====<[ Struct: t_local ]>=======================
 typedef struct s_local
 {
 	char	*name;
 	sem_t	*sem;	
 }				t_local;
 
+//====<[ Struct: t_philo ]>=======================
 typedef struct s_philo
 {
 	int				id;
@@ -103,6 +107,7 @@ typedef struct s_philo
 	pid_t			pid;
 }					t_philo;
 
+//====<[ Struct: t_init ]>========================
 typedef struct s_init
 {
 	t_args			*args;
@@ -111,7 +116,9 @@ typedef struct s_init
 	pthread_t		meals_monitor;
 }					t_init;
 
-	/******** PHILO_INIT *********/
+//====<[ Functions: prototypes ]>===============================================
+
+//====<[ Function: philo_init ]>==================
 t_init		*dining_philosofers_init(t_init *init, int ac, char **av);
 t_args		*get_args(int ac, char **av);
 t_philo		*take_seats_around_table(t_init *init);
@@ -120,7 +127,7 @@ sem_t		*put_forks_on_table(t_init *init);
 int			set_actions(t_philo *p);
 int			start_simulation(t_philo *phs);
 
-	/********** ACTIONS **********/
+//====<[ Function: actions ]>=====================
 int			live_cycle(t_philo *p);
 int			start_eating(t_philo *p);
 int			start_sleeping(t_philo *p);
@@ -128,7 +135,7 @@ int			start_thinking(t_philo *p);
 void		*live_monitor(void *arg);
 void		*meals_monitor(void *arg);
 
-	/*********** Utils ***********/
+//====<[ Function: utils ]>=======================
 int			ft_atoi(char *s);
 char		*ft_itoa(int n);
 t_ullint	get_current_time(void);
@@ -138,4 +145,6 @@ int			ft_strcmp(char *s1, char *s2);
 int			my_usleep(t_ullint start, t_ullint duration);
 int			print_action(t_philo *p, char *action);
 int			clear_lg(t_philo *p);
+
 #endif
+//==============================================================================
